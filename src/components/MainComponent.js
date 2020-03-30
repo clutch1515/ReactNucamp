a288414733a613570e59fd3ab05cd86a08f9b9a6
Peter Larsen <clutch1515@gmail.com>
2020-03-30 08:09:17 -0700

    End of week 3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,12 +4,14 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
+import { PROMOTIONS } from '../shared/promotions'
+
 
 class Main extends Component {
     constructor(props) {
@@ -50,6 +52,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route path='/aboutus' render={() => <About partners={this.state.partners} /> } />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
